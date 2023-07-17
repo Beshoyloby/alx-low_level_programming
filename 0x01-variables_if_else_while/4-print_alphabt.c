b0291f6164eaa6e69c7e268af5f85cfb08a4bf7b
Beshoyloby <beshoysameh35.com>
2023-07-17 13:16:46 -0700

    finishing

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,13 @@
 
 int main(void)
 {
-	char ch = 'z';
+	char ch = 'a';
 
-	while (ch >= 'a')
-
+	while (ch <= 'z')
 {
+	if (ch == 'e' || ch == 'q')
+	ch++;
 	putchar(ch);
-	ch--;
 }
 	putchar('\n');
 	return (0);
